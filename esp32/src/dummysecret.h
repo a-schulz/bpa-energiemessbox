71da002c6a57172df8a90058e660576209688a36
magnus.boehne <magnus.boehne@gmail.com>
2025-06-18 09:25:16 +0200

    Dummy-Commit

---
+++ esp32/src/dummysecret.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* ssid = "WLAN NAME";
+const char* password = "WLAN PASSWORDT";
+
+const char* mqtt_server = "MQTT IP";
+const int mqtt_port = 1;   // PORT                 
+const char* mqtt_topic = "Person in Area Topic"; ///
+const char* mqtt_topic2 = "Danger Topic";
+
